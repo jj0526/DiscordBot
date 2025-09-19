c9bdd783f1a765489d924c023b02496d5f877b27
jj <syeonie0526@gmail.com>
2025-09-20 03:23:38 +0900

    fix: load_extension()를 사용해 코그 등록하도록 수정

--- main.py
+++ main.py
@@ -2,35 +2,22 @@ import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
+import asyncio
 
 load_dotenv()
-
-# Import the roll_dice cog
-from roll_dice import roll_dice
-
-# Import the music_cog
-from music_cog import music_cog
-from help_cog import help_cog
-from quality_cog import quality_cog
 
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="?", intents=intents)
-
-@bot.event
-async def on_ready():
-    await bot.add_cog(music_cog(bot))
-    await bot.add_cog(roll_dice(bot))
-    await bot.add_cog(help_cog(bot))
-    await bot.add_cog(quality_cog(bot))
-    print(f"{bot.user.name} has connected to Discord!")
-
 bot.remove_command('help')
-# Remove the default help command so that we can write our own
 
-# Start the bot with our token
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
-bot.run(TOKEN)
+async def main():
+    await bot.load_extension("music_cog")
+    await bot.load_extension("roll_dice")
+    await bot.load_extension("help_cog")
+    await bot.load_extension("quality_cog")
+    await bot.start(os.getenv('DISCORD_TOKEN'))
+
+asyncio.run(main())
